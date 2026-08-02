b88d9d679185e5f18b6e5c941fc28bc90cbe0a7a
Oleksandr Tishchenko <dev@smartresponsor.com>
2026-08-02 13:32:38 -0500

    Expose canonical Ordering bundle class

--- src/OrderingBundle.php
+++ src/OrderingBundle.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace App;
+namespace App\Ordering;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class OrderComponentBundle extends Bundle
+final class OrderingBundle extends Bundle
 {
 }
